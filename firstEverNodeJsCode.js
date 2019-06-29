5b56e4c34d432c890d8cba7c81db52105ed2940c
Hams Ahmed Ansari <35776235+hamsahmedansari@users.noreply.github.com>
2019-06-29 14:25:40 +0500

    Delete firstEverNodeJsCode.js

--- firstEverNodeJsCode.js
+++
@@ -1,5 +0,0 @@
-console.log("This is my first ever Node JS code!!");
-
-//To run this code goto to command prompt and run
-// $ node firstEverNodeJsCode
-// Output will be: This is my first ever Node JS code!!

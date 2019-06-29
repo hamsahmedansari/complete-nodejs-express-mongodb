b61c91f2b0940e920fda10f17f243a41a6d334a2
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-29 14:40:11 +0500

    Starter Templete

--- index.js
+++
@@ -1,9 +0,0 @@
-// Validator
-const validator = require("validator");
-
-console.log(validator.isEmail("aamirpinger@yahoo.com"));
-
-// Chalk
-const chalk = require("chalk");
-
-console.log(chalk.bgGreen.red.bold("Hello world!"));

d570c37f1a22b589c10c417d2b8c49f6435697f7
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-29 16:20:54 +0500

    Asynchronous Requests

--- SimpleAsyncRequest.js
+++ SimpleAsyncRequest.js
@@ -1,12 +1,3 @@
-# Asynchronous Requests
-
-1. Simple Asynchronous Requests
-
-## Asynchronous Requests
-
-### Code
-
-```
 console.log("Testing asynchronous example start");
 
 setTimeout(() => console.log("Async code after a pause of 3 seconds"), 3000);
@@ -14,13 +5,3 @@ setTimeout(() => console.log("Async code after a pause of 3 seconds"), 3000);
 setTimeout(() => console.log("Async after a pause of 0 seconds"), 0);
 
 console.log("Testing asynchronous example ends");
-
-```
-
-### Run
-
-`node SimpleAsyncRequest.js`
-
-### Output
-
-![](https://i.imgur.com/FFrwmbI.png)

ab3e4d286cdea23581ec482a2682da2496b16fcc
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-29 14:11:34 +0500

    Update: :construction: code added  4 is Created :  1 : firstEverNodeJsCode.js  2 : package.json  3 :  4 : nothingaddedtocommitbutuntrackedfilespresent(usegitaddtotrack

---
+++ firstEverNodeJsCode.js
@@ -0,0 +1,5 @@
+console.log("This is my first ever Node JS code!!");
+
+//To run this code goto to command prompt and run
+// $ node firstEverNodeJsCode
+// Output will be: This is my first ever Node JS code!!

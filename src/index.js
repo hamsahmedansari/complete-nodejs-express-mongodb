1b8bf606ba9ebb775837830b4e475b33fdff3017
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-30 17:24:56 +0500

    Update / Patch

--- src/index.js
+++ src/index.js
@@ -45,6 +45,31 @@ app.get("/profiles/:id", async (req, res) => {
   }
 });
 
+app.patch("/profiles/:id", async (req, res) => {
+  const fieldsToUpdate = Object.keys(req.body);
+  const fieldsInModel = ["name", "age", "graduate", "email"];
+  const isUpdateAllowed = fieldsToUpdate.every(field =>
+    fieldsInModel.includes(field)
+  );
+
+  if (!isUpdateAllowed) {
+    return res.status(400).send({ error: "Invalid fields!" });
+  }
+
+  try {
+    const profile = await Profiles.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!profile) {
+      return res.status(404).send();
+    }
+    res.send(profile);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });

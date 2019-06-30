36b5b0073a4130d13db0739451e93bb30d8b8589
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-06-30 17:22:08 +0500

    Async / await

--- src/promise.js
+++ src/promise.js
@@ -1,20 +1,28 @@
 require("./db/mongoose");
 const Profiles = require("./models/profiles");
 
-Profiles.findByIdAndUpdate(
-  {
-    _id: "5d18a83d27f1bf68fb0aa4b8"
-  },
-  {
-    graduate: false
-  }
-)
-  .then(rec => {
-    console.log(rec);
-    return Profiles.countDocuments({
+const updateRecord = async id => {
+  try {
+    const profile = await Profiles.findByIdAndUpdate(
+      {
+        _id: id
+      },
+      {
+        graduate: false
+      }
+    );
+    console.log(profile);
+    if (!profile) {
+      throw Error("User id does not exist");
+    }
+    const count = await Profiles.countDocuments({
       graduate: false
     });
-  })
-  .then(count => {
     console.log(count);
-  });
+  } catch (e) {
+    console.log("error occurred: ", e);
+  }
+};
+updateRecord("123456789012");
+
+// updateRecord('5d18a83d27f1bf68fb0aa4b8')
